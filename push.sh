0c33552562435f97f4511b80118a0318f507cec6
HOME <15608900169@139.come>
2023-09-14 10:50:39 +0800

    自动推送

--- push.sh
+++ push.sh
@@ -1,9 +1,6 @@
-
-
 git config --global user.name 'HOME'
 git config --global user.email '15608900169@139.come'
 git config --global http.sslverify false
 git add .
 git commit -m 自动推送
-git commit -m自动推送
 git push
